039a9a9410c1bd685ebec9a79f7ad8c0e07233a0
mati <mateusz.labuda@voicetel.pl>
2020-03-29 18:56:23 +0200

    added pl restaurant

---
+++ factory/method/pasta/pl_napoli.py
@@ -0,0 +1,10 @@
+from .pasta import Pasta
+
+
+class PlNapoli(Pasta):
+    def __init__(self, weight: int, parmesan: bool = False, al_dente=False):
+        super().__init__(weight, parmesan, al_dente)
+        self.typ = 'polish'
+
+    def eat(self):
+        print(f"eat {self.__class__.__name__}, attrs: {self.__dict__}")
