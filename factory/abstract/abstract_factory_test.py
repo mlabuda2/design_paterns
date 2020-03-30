dd8a7feac7507305c586099eabdae010d6f9e33d
mati <mateusz.labuda@voicetel.pl>
2020-03-30 22:11:10 +0200

    added performance tests

--- factory/abstract/abstract_factory_test.py
+++ factory/abstract/abstract_factory_test.py
@@ -1,8 +1,25 @@
 from factory.abstract.pasta.bolognese_factory import BologneseFactory
 from factory.abstract.pizza.pepperoni_factory import PepperoniFactory
 from factory.abstract.seafood.clams_factory import ClamsFactory
+from factory.abstract.pasta.bolognese_factory_reflection import BologneseFactoryReflection
+from factory.abstract.pizza.pepperoni_factory_reflection import PepperoniFactoryReflection
+from factory.abstract.seafood.clams_factory_reflection import ClamsFactoryReflection
 
-if __name__ == '__main__':
+
+def test_factory_with_reflection():
+    bolognese_factory = BologneseFactoryReflection()
+    bolognese = bolognese_factory.cook(111, True)
+    bolognese.eat()
+
+    clams_factory = ClamsFactoryReflection()
+    clams = clams_factory.cook(10, None)
+    clams.eat()
+
+    pepperoni_factory = PepperoniFactoryReflection()
+    pepperoni = pepperoni_factory.cook_double_dough(33, 'tomato')
+    pepperoni.eat()
+
+def test_factory():
     bolognese_factory = BologneseFactory()
     bolognese = bolognese_factory.cook(111, True)
     bolognese.eat()
@@ -14,3 +31,11 @@ if __name__ == '__main__':
     pepperoni_factory = PepperoniFactory()
     pepperoni = pepperoni_factory.cook_double_dough(33, 'tomato')
     pepperoni.eat()
+
+
+if __name__ == '__main__':
+    # for _ in range(100000):
+    #     test_factory_with_reflection()
+
+    for _ in range(100000):
+        test_factory()
