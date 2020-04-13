cbb4ad92bbdda093a823b896cf6e20c59acbc323
mati <mateusz.labuda@voicetel.pl>
2020-04-13 12:56:25 +0200

    init deep prototype

---
+++ prototype/deep_prototype/volume_manager.py
@@ -0,0 +1,12 @@
+from prototype.deep_prototype.volume import Volume
+
+
+class VolumeManager:
+    def __init__(self) -> None:
+        self._volume_database = {}
+
+    def get_by_ident(self, ident: int) -> Volume:
+        return self._volume_database[ident]
+
+    def add(self, idx: int, volume: Volume) -> None:
+        self._volume_database.update({idx: volume})
